ccc35011c16a6c6a9e57f2c0e81b2b73cb559022
Max Polzin <polzin.max@gmail.com>
2024-05-06 17:52:14 +0200

    Update ports.

--- PoluluPwmControl/PoluluPwmControl.ino
+++ PoluluPwmControl/PoluluPwmControl.ino
@@ -1,7 +1,7 @@
 const uint8_t numChannels = 4;
 
-const uint8_t input_pins[numChannels] = {5, 6, 7, 8};
-const uint8_t output_pins[numChannels] = {9, 10, 11, 12};
+const uint8_t input_pins[numChannels] = {2, 4, 8, 10};
+const uint8_t output_pins[numChannels] = {3, 5, 9, 11};
 
 const unsigned long minPulseWidth = 1000;
 const unsigned long maxPulseWidth = 2000;
