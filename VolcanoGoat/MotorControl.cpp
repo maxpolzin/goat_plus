de21652a875d94dc2459510dfe311a8a7756dde6
Max Polzin <polzin.max@gmail.com>
2024-08-13 17:19:46 +0200

    Integrate wheel control.

--- VolcanoGoat/MotorControl.cpp
+++ VolcanoGoat/MotorControl.cpp
@@ -19,33 +19,27 @@ void MotorControl::begin(int leftPin, int rightPin, int winchPin, int cameraPin)
 }
 
 void MotorControl::update(double forwardVelocityCommand, double steeringVelocityCommand, double winchVelocityCommand, double cameraPositionCommand) {
-  // double leftWheelCommand = forwardVelocityCommand + steeringVelocityCommand;
-  // double rightWheelCommand = forwardVelocityCommand - steeringVelocityCommand;
+  double leftWheelCommand = forwardVelocityCommand + steeringVelocityCommand;
+  double rightWheelCommand = forwardVelocityCommand - steeringVelocityCommand;
 
-  // double maxValue = max(max(abs(leftWheelCommand), abs(rightWheelCommand)), 1.0);
+  double maxValue = max(max(abs(leftWheelCommand), abs(rightWheelCommand)), 1.0);
 
-  // leftWheelCommand = leftWheelCommand / maxValue;
-  // rightWheelCommand = rightWheelCommand / maxValue;
+  leftWheelCommand = leftWheelCommand / maxValue;
+  rightWheelCommand = rightWheelCommand / maxValue;
 
-  // int leftMotorPWM = map(leftWheelCommand, -1.0, 1.0, SERVO_MIN, SERVO_MAX);
-  // int rightMotorPWM = map(rightWheelCommand, -1.0, 1.0, SERVO_MIN, SERVO_MAX);
+  int leftMotorPulsewidth = map(leftWheelCommand*1000, -1000, 1000, SERVO_MIN, SERVO_MAX);
+  int rightMotorPulsewidth = map(rightWheelCommand*1000, -1000, 1000, SERVO_MIN, SERVO_MAX);
 
-  // int winchPWM = map(winchVelocityCommand*1000, -1000, 1000, SERVO_MIN, SERVO_MAX);
+  uint16_t leftMotorDutyCycle = map(leftMotorPulsewidth, 0, 1e6/PWM_BASE_FREQ, 0, 4095);
+  uint16_t rightMotorDutyCycle = map(rightMotorPulsewidth, 0, 1e6/PWM_BASE_FREQ, 0, 4095);
 
-  // uint16_t leftMotorSignal = map(leftMotorPWM, 0, 3000, 0, 255);
-  // uint16_t rightMotorSignal = map(rightMotorPWM, 0, 3000, 0, 255);
+  ledcAnalogWrite(PWM_LEFT_CHANNEL, leftMotorDutyCycle, 4095);
+  ledcAnalogWrite(PWM_RIGHT_CHANNEL, rightMotorDutyCycle, 4095);
 
-  // uint16_t winchMotorSignal = map(winchPWM, 0, 3000, 0, 255);
-
-  // ledcAnalogWrite(PWM_LEFT_CHANNEL, leftMotorSignal);
-  // ledcAnalogWrite(PWM_RIGHT_CHANNEL, rightMotorSignal);
-
-  // ledcAnalogWrite(PWM_WINCH_CHANNEL, winchMotorSignal);
 
   int winchPulsewidth = map(winchVelocityCommand*1000, -1000, 1000, WINCH_SERVO_MIN, WINCH_SERVO_MAX);  
   uint16_t winchServoDutyCycle = map(winchPulsewidth, 0, 1e6/PWM_WINCH_BASE_FREQ, 0, 4095);
   ledcAnalogWrite(PWM_WINCH_CHANNEL, winchServoDutyCycle, 4095);
-
 
   int cameraPulsewidth = map(cameraPositionCommand*1000, -1000, 1000, CAMERA_SERVO_MIN, CAMERA_SERVO_MAX);  
   uint16_t cameraServoDutyCycle = map(cameraPulsewidth, 0, 1e6/PWM_CAMERA_BASE_FREQ, 0, 4095);
