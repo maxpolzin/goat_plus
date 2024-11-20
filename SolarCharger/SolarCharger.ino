0677e4fe5be337c0507173a3c694c9f48908de33
Max Polzin <polzin.max@gmail.com>
2024-11-20 17:32:16 +0100

    Add second potentiometer to monitor output.

--- SolarCharger/SolarCharger.ino
+++ SolarCharger/SolarCharger.ino
@@ -8,7 +8,8 @@
 #define PIN_SG90 16
 
 TwoWire twoWire = TwoWire(0);
-Adafruit_INA219 ina219;
+Adafruit_INA219 ina219_pv(0x40);  // Sensor for PV panel
+Adafruit_INA219 ina219_battery(0x41);  // Sensor for battery
 
 Potentiometer potentiometer(PIN_SG90);
 MPPT mppt;
@@ -18,11 +19,20 @@ void setup() {
     Serial.begin(115200);
 
     twoWire.setPins(I2C_SDA, I2C_SCL);
-    if (!ina219.begin(&twoWire)) {
-        Serial.println("Failed to find INA219 chip");
+    
+
+    if (!ina219_pv.begin(&twoWire)) {  // Default address for first INA219
+        Serial.println("Failed to find INA219 chip for PV panel");
         while (1) { delay(10); }
     }
-    Serial.println("Measuring voltage and current with INA219 ...");
+    Serial.println("Initialized INA219 for PV panel");
+
+    if (!ina219_battery.begin(&twoWire)) {  // Secondary address for second INA219
+        Serial.println("Failed to find INA219 chip for battery");
+        while (1) { delay(10); }
+    }
+    Serial.println("Initialized INA219 for battery");
+
 
     potentiometer.setup(); // Setup potentiometer (SG90 servo)
 }
@@ -30,13 +40,19 @@ void setup() {
 void loop() {
     delay(2000);
 
-    float current_mA = ina219.getCurrent_mA();
-    float loadvoltage = ina219.getBusVoltage_V() + (ina219.getShuntVoltage_mV() / 1000);
+    float pv_current_mA = ina219_pv.getCurrent_mA();
+    float pv_voltage_V = ina219_pv.getBusVoltage_V() + (ina219_pv.getShuntVoltage_mV() / 1000);
 
-    Serial.printf("Current: %.3f mA, Voltage: %.3f V\n", current_mA, loadvoltage);
+    float battery_current_mA = ina219_battery.getCurrent_mA();
+    float battery_voltage_V = ina219_battery.getBusVoltage_V() + (ina219_battery.getShuntVoltage_mV() / 1000);
 
-    float current_limit = mppt.update(loadvoltage, current_mA);
-    potentiometer.setCurrent(current_limit);
+    Serial.printf("PV Panel -> Current: %.3f mA, Voltage: %.3f V\n", pv_current_mA, pv_voltage_V);
 
-    Serial.printf("Adjusted Current Limit: %.3f mA\n", current_limit);
+    Serial.printf("Battery  -> Current: %.3f mA, Voltage: %.3f V\n", battery_current_mA, battery_voltage_V);
+
+
+    float charging_current_limit = mppt.update(pv_voltage_V, pv_current_mA);
+    potentiometer.setCurrent(charging_current_limit);
+
+    Serial.printf("Adjusted Charging Current Limit: %.3f mA\n", charging_current_limit);
 }
